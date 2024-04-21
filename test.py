264c8b24e0b08615cdcf4b819d71d88d9ab696e3
Soumya21103 <soumya21103@iiitd.ac.in>
2024-04-21 22:38:14 +0530

    m commit

--- test.py
+++
@@ -1,8 +0,0 @@
-import mapper_pb2  as mapper_pb2
-import mapper_pb2_grpc
-import reduce_pb2 as rd2
-import reduce_pb2_grpc as rd2g
-import master_pb2 as ms2
-import master_pb2_grpc as ms2g
-
-with grpc.insec
